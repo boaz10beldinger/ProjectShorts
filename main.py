5445d30bbea84402a5a4c5b98a6c61801727c713
Mathanhannes <mathanhannes@gmail.com>
2023-10-01 10:37:27 +0300

    testing commit

---
+++ main.py
@@ -0,0 +1,118 @@
+from google.cloud import speech_v1p1beta1 as speech
+from google.cloud import speech
+
+
+def transcribe_gcs_with_word_time_offsets(
+    gcs_uri: str,
+) -> speech.RecognizeResponse:
+    """Transcribe the given audio file asynchronously and output the word time
+    offsets."""
+    from google.cloud import speech
+
+    client = speech.SpeechClient()
+
+    audio = speech.RecognitionAudio(uri=gcs_uri)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=16000,
+        language_code="en-US",
+        enable_word_time_offsets=True,
+    )
+
+    operation = client.long_running_recognize(config=config, audio=audio)
+
+    print("Waiting for operation to complete...")
+    result = operation.result(timeout=90)
+
+    for result in result.results:
+        alternative = result.alternatives[0]
+        print(f"Transcript: {alternative.transcript}")
+        print(f"Confidence: {alternative.confidence}")
+
+        for word_info in alternative.words:
+            word = word_info.word
+            start_time = word_info.start_time
+            end_time = word_info.end_time
+
+            print(
+                f"Word: {word}, start_time: {start_time.total_seconds()}, end_time: {end_time.total_seconds()}"
+            )
+
+    return result
+
+def stt_recognize_speaker():
+    client = speech.SpeechClient()
+
+    speech_file = "resources/commercial_mono.wav"
+
+    with open(speech_file, "rb") as audio_file:
+        content = audio_file.read()
+
+    audio = speech.RecognitionAudio(content=content)
+
+    diarization_config = speech.SpeakerDiarizationConfig(
+        enable_speaker_diarization=True,
+        min_speaker_count=2,
+        max_speaker_count=10,
+    )
+
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=8000,
+        language_code="en-US",
+        diarization_config=diarization_config,
+    )
+
+    print("Waiting for operation to complete...")
+    response = client.recognize(config=config, audio=audio)
+
+    # The transcript within each result is separate and sequential per result.
+    # However, the words list within an alternative includes all the words
+    # from all the results thus far. Thus, to get all the words with speaker
+    # tags, you only have to take the words list from the last result:
+    result = response.results[-1]
+
+    words_info = result.alternatives[0].words
+
+    # Printing out the output:
+    for word_info in words_info:
+        print(f"word: '{word_info.word}', speaker_tag: {word_info.speaker_tag}")
+
+    return result
+
+def transcribe_gcs(gcs_uri: str) -> str:
+    """Asynchronously transcribes the audio file specified by the gcs_uri.
+
+    Args:
+        gcs_uri: The Google Cloud Storage path to an audio file.
+
+    Returns:
+        The generated transcript from the audio file provided.
+    """
+
+    client = speech.SpeechClient()
+
+    audio = speech.RecognitionAudio(uri=gcs_uri)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=44100,
+        language_code="en-US",
+    )
+
+    operation = client.long_running_recognize(config=config, audio=audio)
+
+    print("Waiting for operation to complete...")
+    response = operation.result(timeout=90)
+
+    transcript_builder = []
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        transcript_builder.append(f"\nTranscript: {result.alternatives[0].transcript}")
+        transcript_builder.append(f"\nConfidence: {result.alternatives[0].confidence}")
+
+    transcript = "".join(transcript_builder)
+    print(transcript)
+
+    return transcript
